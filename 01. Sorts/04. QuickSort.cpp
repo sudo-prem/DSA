c0d5af0ab5116595995b478218014043a1b18fe5
Premnaath <premnaathvaradharajan@gmail.com>
2021-05-05 17:28:05 +0530

    Modify '01. Sorts/04. Quicksort.cpp'

--- 01. Sorts/04. QuickSort.cpp
+++ 01. Sorts/04. QuickSort.cpp
@@ -21,22 +21,27 @@ void QuickSort(vector<int> &arr, int i, int j)
                 swap(arr[i], arr[j]);
         } while (i < j);
         swap(arr[pivot], arr[j]);
+        // Here the jth element which is already in its sorted postion,
+        // acts as the last dummy element for the left side QuickSort
         QuickSort(arr, pivot, j);
+        // since end still holds the last dummy element we can directly pass it
         QuickSort(arr, j + 1, end);
     }
 }
 
 void QuickSort(vector<int> &arr)
 {
-    QuickSort(arr, 0, arr.size());
+    QuickSort(arr, 0, arr.size() - 1);
 }
 
 int main()
 {
-    vector<int> arr{5, 4, 3, 2};
+    // Last element is taken as dummy element
+    vector<int> arr{8, 2, 4, 1, 3, -6, 7, 8, INT_MAX};
     QuickSort(arr);
-    for (auto i : arr)
-        cout << i << " ";
+    // Printing all the elements except the last dummy element
+    for (int i = 0; i < arr.size() - 1; ++i)
+        cout << arr[i] << " ";
     cout << endl;
     return 0;
 }
