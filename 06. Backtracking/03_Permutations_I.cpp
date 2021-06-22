e51217da15d981f3d606e4f7999a02faa04081f9
Premnaath <premnaathvaradharajan@gmail.com>
2021-06-22 22:44:26 +0530

    Update 03_Permutations_1.cpp with optimal solution

--- 06. Backtracking/03_Permutations_I.cpp
+++ 06. Backtracking/03_Permutations_I.cpp
@@ -1,8 +1,13 @@
 // Problem Link:
 // https://leetcode.com/problems/permutations/
 
-// TC: O(n * (2^n))
-// SC: O(2^n)
+// Approach 1: (Searching)
+// TC: O((n^2)*n!)
+// TC: O(1)
+
+// Approach 2: (Swapping)
+// TC: O(n!)
+// TC: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -10,45 +15,85 @@ using namespace std;
 #define ll long long
 #define deb(x) cout << #x << ": " << x << "\n"
 
-void helper(vector<int> &nums, vector<int> &curr, vector<vector<int>> &res)
+// Approach 1
+void helper_1(vector<int> &nums, vector<int> &curr, vector<vector<int>> &res)
 {
     if (curr.size() == nums.size())
     {
         res.push_back(curr);
         return;
     }
+
     for (int i = 0; i < nums.size(); ++i)
     {
-        if (find(curr.begin(), curr.end(), nums[i]) != curr.end())
-            continue;
-
-        curr.push_back(nums[i]);
-        helper(nums, curr, res);
-        curr.pop_back();
+        if (find(curr.begin(), curr.end(), nums[i]) == curr.end())
+        {
+            curr.push_back(nums[i]);
+            helper_1(nums, curr, res);
+            curr.pop_back();
+        }
     }
 }
 
-vector<vector<int>> permute(vector<int> &nums)
+vector<vector<int>> permute_1(vector<int> &nums)
 {
     vector<int> curr;
     vector<vector<int>> res;
 
-    helper(nums, curr, res);
+    helper_1(nums, curr, res);
+    return res;
+}
+
+// Approach 2
+void helper_2(vector<int> &nums, int index, vector<vector<int>> &res)
+{
+    if (index == nums.size())
+    {
+        res.push_back(nums);
+        return;
+    }
+
+    for (int i = index; i < nums.size(); ++i)
+    {
+        swap(nums[index], nums[i]);
+        helper_2(nums, index + 1, res);
+        swap(nums[index], nums[i]);
+    }
+}
+
+vector<vector<int>> permute_2(vector<int> &nums)
+{
+    vector<vector<int>> res;
+    int index{};
+
+    helper_2(nums, index, res);
     return res;
 }
 
 void solve()
 {
     vector<int> nums{1, 2, 3};
-    vector<vector<int>> res = permute(nums);
+    vector<vector<int>> res;
 
+    res = permute_1(nums);
     for (auto i : res)
     {
         cout << "[ ";
         for (auto j : i)
             cout << j << " ";
-        cout << "]\n";
+        cout << "] ";
     }
+    cout << endl;
+
+    res = permute_2(nums);
+    for (auto i : res)
+    {
+        cout << "[ ";
+        for (auto j : i)
+            cout << j << " ";
+        cout << "] ";
+    }
+    cout << endl;
 }
 
 int main()
