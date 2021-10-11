feba859b9214bfab589f47bfd8fa725bf97e815b
Premnaath <premnaathvaradharajan@gmail.com>
2021-10-11 21:56:49 +0530

    Update 05_DP

--- 05_DP/03_Longest_Common_Subsequence_Variations/01_Longest_Common_Subsequence.cpp
+++ 05_DP/03_Longest_Common_Subsequence_Variations/01_Longest_Common_Subsequence.cpp
@@ -75,6 +75,29 @@ int longestCommonSubsequence3(string text1, string text2)
     return dp[l1][l2];
 }
 
+// Approach 4: (Bottom - Up)
+// Tabulation with O(n) Space
+int longestCommonSubsequence4(string text1, string text2)
+{
+    int n = text1.size();
+    int m = text2.size();
+
+    vector<vector<int>> dp(2, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= m; ++j)
+        {
+            if (text1[i - 1] == text2[j - 1])
+                dp[i % 2][j] = 1 + dp[(i + 1) % 2][j - 1];
+            else
+                dp[i % 2][j] = max(dp[(i + 1) % 2][j], dp[i % 2][j - 1]);
+        }
+    }
+
+    return dp[n % 2][m];
+}
+
 void solve()
 {
     string text1 = "abcde";
@@ -84,6 +107,7 @@ void solve()
     cout << longestCommonSubsequence1(text1, text2) << endl;
     cout << longestCommonSubsequence2(text1, text2) << endl;
     cout << longestCommonSubsequence3(text1, text2) << endl;
+    cout << longestCommonSubsequence4(text1, text2) << endl;
 }
 
 int main()
