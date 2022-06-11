0dcf997829a49647ef2f3c737b93f7427206922b
premnaaath <premnaathvaradharajan@gmail.com>
2022-06-11 23:19:17 +0530

    Complete BinarySearch

--- 11_Binary_Search/19_Allocate_Pages.cpp
+++ 11_Binary_Search/19_Allocate_Pages.cpp
@@ -1,27 +1,79 @@
+// Problem Link:
+// https://www.interviewbit.com/problems/allocate-books/
+
+// TC: O(log(sum(books)))
+// SC: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
-#define pii pair<int, int>
 #define deb(x) cout << #x << ": " << x << "\n"
+
+bool isPossible(vector<int> &books, int students, int currLimit)
+{
+	int currStudents{};
+	int pages{};
+
+	for (int i : books)
+	{
+		if (i > currLimit)
+			return false;
+		if (pages + i > currLimit)
+		{
+			pages = i;
+			currStudents++;
+		}
+		else
+			pages += i;
+	}
+
+	if (currStudents < students)
+		return true;
+	return false;
+}
+
+int booksMax(vector<int> &books, int students)
+{
+	int n = books.size();
+	// We cannot allocate books, if number
+	// of books is lesser than students
+	if (students > n)
+		return -1;
+
+	// Defining the search space for binary search
+	int low = 0, high = INT_MAX;
+	int res{-1}, currLimit;
+
+	while (low <= high)
+	{
+		currLimit = low + (high - low) / 2;
+		if (isPossible(books, students, currLimit))
+		{
+			res = currLimit;
+			high = currLimit - 1;
+		}
+		else
+			low = currLimit + 1;
+	}
+
+	return res;
+}
 
 void solve()
 {
-	
+	vector<int> books{12, 34, 67, 90};
+	int students = 2;
+
+	cout << booksMax(books, students) << endl;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0), cin.tie(0);
-#ifdef ONPC
-	freopen("input.txt", "r", stdin);
-#endif
-	int t {1};
-	/* int i {1}; cin >> t; */
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t{1};
+	// cin >> t;
 	while (t--)
-	{
-		/* cout << "Case #" << i++ << ": "; */
 		solve();
-	}
 	return 0;
 }
-
--- 11_Binary_Search/20_Aggressive_Cows.cpp
+++ 11_Binary_Search/20_Aggressive_Cows.cpp
@@ -1,27 +1,85 @@
+// Problem Link:
+// https://www.spoj.com/problems/AGGRCOW/
+
+// TC: O(n*log(n))
+// SC: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
-#define pii pair<int, int>
 #define deb(x) cout << #x << ": " << x << "\n"
+
+bool isPossible(vector<int> &stalls, int cows, int currLimit)
+{
+    int currCows{1}, currStall{stalls[0]};
+    int n = stalls.size();
+
+    for (int i = 1; i < n; ++i)
+    {
+        if (stalls[i] - currStall >= currLimit)
+        {
+            currCows++;
+            currStall = stalls[i];
+        }
+
+        if (currCows >= cows)
+            return true;
+    }
+
+    return false;
+}
+
+int largestMinDistance(vector<int> &stalls, int cows)
+{
+    if (cows > stalls.size())
+        return -1;
+    int n = stalls.size(), res{0};
+
+    // Defining the search space
+    int minValue{1}, maxValue = stalls[n - 1] - stalls[0];
+
+    // Defining variables for search
+    int low = minValue, high = maxValue, currLimit{};
+    while (low <= high)
+    {
+        currLimit = low + (high - low) / 2;
+
+        if (isPossible(stalls, cows, currLimit))
+        {
+            res = currLimit;
+            low = currLimit + 1;
+        }
+        else
+            high = currLimit - 1;
+    }
+
+    return res;
+}
 
 void solve()
 {
-	
+    int n, cows, temp;
+    cin >> n >> cows;
+
+    vector<int> stalls;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> temp;
+        stalls.push_back(temp);
+    }
+
+    sort(stalls.begin(), stalls.end());
+    cout << largestMinDistance(stalls, cows) << endl;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0), cin.tie(0);
-#ifdef ONPC
-	freopen("input.txt", "r", stdin);
-#endif
-	int t {1};
-	/* int i {1}; cin >> t; */
-	while (t--)
-	{
-		/* cout << "Case #" << i++ << ": "; */
-		solve();
-	}
-	return 0;
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t{1};
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
 }
 
